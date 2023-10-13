0852f10453e6187102bd1716d397c5c11d2b125f
nengelmann <mail@nico-engelmann.de>
2023-10-13 12:40:15 +0200

    add PluginRegestry

--- src/meetingcam/plugins/plugin_utils.py
+++ src/meetingcam/plugins/plugin_utils.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from pathlib import Path
+from typing import  Any
 
-import cv2
 import depthai
 from constants import DEPTHAI, WEBCAM
 from numpy.typing import NDArray
@@ -11,9 +11,9 @@ class PluginBase(ABC):
     """PluginBase class acts as a base class for plugins with image processing capabilities.
 
     This class is intended to be subclassed, with subclasses implementing the abstract
-    `process` method to perform specific image processing tasks.
+    `__init__` and `process` method to perform image processing tasks.
 
-    Attributes:
+    Args:
         model_dir --- directory where models are stored. Defaults to "./src/meetingcam/models".
     """
 
@@ -28,7 +28,7 @@ class PluginBase(ABC):
             With this custom arguments can be passed to plugins, e.g. --name YourName
         """
         self.model_dir = "./src/meetingcam/models"
-        self.type = WEBCAM
+        self.type = WEBCAM # defaults to webcam
 
     @abstractmethod
     def process(
@@ -39,7 +39,7 @@ class PluginBase(ABC):
     ) -> NDArray[Any]:
         """Process an image through the plugin's image processing method.
 
-        This method is abstract and should be overridden in subclasses to implement
+        This method is abstract and should be overwritten in subclasses to implement
         specific image processing functionality.
 
         Args:
@@ -86,3 +86,50 @@ class PluginDepthai(PluginBase):
             device --- depthai device
         """
         pass
+
+
+class PluginRegistry:
+    """PluginRegistry class acts as registry for default and newly added plugins."""
+    def __init__(self):
+        self.plugins = self.register_plugins()
+
+    def register_plugins(self, path: str = "src/meetingcam/plugins") -> list:
+        """Check plugin directory for plugins and return list with found and valid plugins."""
+        dirs = [d for d in Path(path).iterdir() if d.is_dir()]
+        dirs = self._sortout(dirs)
+        plugins = []
+
+        for dir in dirs:
+            plugin_name = dir.name
+            valid = self._check_plugin(dir)
+
+            if valid:
+                plugins.append(plugin_name)
+            else:
+                print(
+                    f"Plugin {plugin_name} is not valid. Continue without"
+                    " registering this plugin."
+                )
+
+        return plugins
+
+    def _check_plugin(self, path: str) -> bool:
+        # TODO: Implement checks to validate plugin
+        return True
+
+    def _sortout(self, dirs: list) -> list:
+        """Sort out directories which start with dot or underscores."""
+        for d in dirs:
+            if str(d.name).startswith(".") or str(d.name).startswith("__"):
+                dirs.remove(d)
+        return dirs
+
+    def import_plugin(self, modulename, name):
+        """Import a named object from a module in the context of this function.
+        Code adapted from: https://www.oreilly.com/library/view/python-cookbook/0596001673/ch15s04.html
+        """
+        try:
+            module = __import__(modulename, globals(), locals(), [name])
+        except ImportError:
+            return None
+        return vars(module)[name]
