2fc526ed9907321c5d716329ec8275d9497db2ee
nengelmann <mail@nico-engelmann.de>
2023-10-13 11:37:27 +0200

    restructuring

--- src/meetingcam/main.py
+++ src/meetingcam/main.py
@@ -1,140 +1,175 @@
 #!/usr/bin/env python3
 
-"""This script is a camera processing pipeline that allows for real-time manipulation of video streams,
-so that a modified video stream can be used in web meeting tools like Teams, Meets or Zoom.
-
-The script utilizes is processing of video frames from a real camera device, and output the modified frames to a virtual camera device. 
-A command line interface is provided for user interaction, with keyboard listeners enabling real-time adjustments 
-like color space conversion, image flipping, and face detection functionalities.
+"""This script is the entry point of MeetingCam.
+MeetingCam allows to manage and modify video streams and route them to web meeting tools like Teams, Meets or Zoom.
 """
 
-import cv2
-import pyvirtualcam
-import typer
-from constants import DEPTHAI, WEBCAM
-from device import DepthaiDevice, WebcamDevice
-from plugins.depthai_yolov5_coco.plugin import Yolov5
-from plugins.openvino_face_detection.plugin import FaceDetection
-from plugins.plugin_utils import PluginBase
-from plugins.roboflow_general.plugin import RoboflowDetection
-from utils import ArgumentHandler, InvalidArgumentError, KeyHandler
+import sys
 
-app = typer.Typer()
+import typer
+from constants import DEPTHAI, TYPES, WEBCAM, TypeArgument
+from device import DepthaiDevice, WebcamDevice
+from plugins.plugin_utils import PluginRegistry
+from print import Printer
+
+# define typer main app and tools
+app = typer.Typer(add_completion=False)
+registry = PluginRegistry()
+printer = Printer()
+
+# register plugins
+for name in registry.plugins:
+    plugin_app = registry.import_plugin(f"plugins.{name}.main", "plugin_app")
+    help = plugin_app.info.help if type(plugin_app.info.help) is str else None
+    short_help = (
+        plugin_app.info.short_help
+        if type(plugin_app.info.short_help) is str
+        else None
+    )
+    plugin_name = (
+        plugin_app.info.name if type(plugin_app.info.help) is str else name
+    )
+    app.add_typer(
+        plugin_app, name=plugin_name, help=help, short_help=short_help
+    )
+
+
+# app entry point (main typer app)
+@app.callback(
+    invoke_without_command=True,
+    epilog=Printer.epilog(),
+    no_args_is_help=False,
+    help=(
+        "AI and CV webcam utility for online meetings.\n\nRun your artificial"
+        " intelligence and computer vision algorithems in online meetings such"
+        " as Zoom, Meets or Teams! 🪄"
+    ),
+    context_settings={
+        "help_option_names": ["-h", "--help"],
+    },
+)
+def main(ctx: typer.Context):
+    """
+    Typer app entry point.
+    Print title, subtitle or help, depending on context.
+    """
+    for plugin in app.registered_groups:
+        if ctx.invoked_subcommand == plugin.name:
+            Printer.subtitle(plugin.name)
+
+    if ctx.invoked_subcommand is None:
+        Printer.title()
+        ctx.get_help()
+
+
+# General-Commands
+@app.command(
+    help="List all camera devices",
+    context_settings={"help_option_names": ["-h", "--help"]},
+    rich_help_panel="General-Commands",
+)
+def list_devices(type: TYPES = TypeArgument):
+    """List all camera devices, camera paths and their virtual counter part."""
+
+    type = str(type.value)
+    if type == DEPTHAI or type == "all":
+        depthai_handler = DepthaiDevice(pipeline=None)
+        printer.console.print(
+            "\nAvailable depthai devices:", style="bold underline"
+        )
+        printer.available_devices(
+            depthai_handler.mapping, depthai_handler.available_devices_real
+        )
+    if type == WEBCAM or type == "all":
+        webcam_handler = WebcamDevice()
+        printer.console.print(
+            "\nAvailable webcam devices:", style="bold underline"
+        )
+        printer.available_devices(
+            webcam_handler.mapping, webcam_handler.available_devices_real
+        )
 
 
 @app.command(
-    context_settings={"allow_extra_args": True, "ignore_unknown_options": True}
+    help="List commands to add camera devices",
+    context_settings={"help_option_names": ["-h", "--help"]},
+    rich_help_panel="General-Commands",
 )
-def main(
-    ctx: typer.Context,
-    device_path: str | None = None,
-    depthai: bool = False,
-    roboflow: bool = False,
-) -> None:
-    """Initialize the several handlers, real and virtual camera devices for video frame capture and processing and run the whole pipeline.
-    It integrates keyboard listeners for real-time user inputs to manipulate video streams.
+def add_devices(type: TYPES = TypeArgument):
+    """List commands on how to add camera devices to be used with MeetingCam."""
 
-    Args:
-        ctx (typer.Context): Context information from the command line.
-        device_path (str | None): The device path for the real camera. Defaults to None.
-    """
+    type = str(type.value)
+    depthai_handler = DepthaiDevice(pipeline=None)
+    webcam_handler = WebcamDevice()
 
-    # preprocess extra arguments, e.g. for custom plugin
-    extra_args = ArgumentHandler(ctx.args)
-    extra_args.print(ctx.args)
+    if depthai_handler.mapping or webcam_handler.mapping:
+        printer.console.print("\nThere are already virtual devices available!")
+        reset_devices()
+        sys.exit(0)
 
-    if depthai:
-        # define plugin
-        plugin = Yolov5()
+    if type == DEPTHAI or type == "all":
+        if len(depthai_handler.available_devices_real[0]) > 0:
+            printer.console.print(
+                "\nAdd depthai devices:", style="bold underline"
+            )
+            printer.add_virtual_devices(
+                depthai_handler.available_devices_real, DEPTHAI
+            )
+        else:
+            printer.console.print(
+                "\nNo depthai device available. Make sure to connect a depthai"
+                " device to your PC.\n",
+                style=printer.warning_style,
+            )
+            sys.exit(0)
 
-        # initialize camera device handler with OAK as input device
-        device_handler = DepthaiDevice(plugin.pipeline)
-        virtual_path = device_handler.init_device(device_path)
-
-    elif roboflow:
-        # handle extra argument --name, if defined
-        roboflow_api_key = extra_args.get("--roboflow-api-key", ctx.args)
-        project_name = extra_args.get("--project-name", ctx.args)
-        version = extra_args.get("--version", ctx.args)
-        # define plugin
-        plugin = RoboflowDetection(roboflow_api_key, project_name, version)
-        # initialize camera device handler with webcam as input device
-        device_handler = WebcamDevice()
-        virtual_path = device_handler.init_device(device_path)
-
-    else:
-        # handle extra argument --name, if defined
-        name = extra_args.get("--name", ctx.args)
-        # define plugin
-        plugin = FaceDetection(name=name)
-        # initialize camera device handler with webcam as input device
-        device_handler = WebcamDevice()
-        virtual_path = device_handler.init_device(device_path)
-
-    # perform some checks on given configuration before camera is started
-    if plugin.type is None:
-        raise NotImplementedError(
-            "Your plugin needs to have a plugin type assigned. plugin.type"
-            " WEBCAM | DEPTHAI"
-        )
-    if depthai and plugin.type == WEBCAM:
-        raise InvalidArgumentError(
-            "The cli flag 'depthai' is set but the plugin you run is meant for"
-            " webcam usage."
+    if type == WEBCAM or type == "all":
+        if len(webcam_handler.available_devices_real[0]) > 0:
+            printer.console.print(
+                "\nAdd webcam devices:", style="bold underline"
+            )
+            printer.add_virtual_devices(
+                webcam_handler.available_devices_real, WEBCAM
+            )
+        else:
+            printer.console.print(
+                "\nNo webcam device available. Make sure to connect a webcam"
+                " device to your PC.\n",
+                style=printer.warning_style,
+            )
+            sys.exit(0)
+    if type == DEPTHAI or type == WEBCAM or type == "all":
+        printer.console.print(
+            "\nYou can [bold]reset[/bold] the added [bold]virtual"
+            " devices[/bold] running:\n[bold][cyan]sudo modprobe -r"
+            " v4l2loopback[/cyan][/bold]\nIf you want to run a reset, please"
+            " close all applications which might access a camera device (e.g."
+            " your browser) before running this command.\nIf you previously"
+            " added devices, a reset is necessary before adding a new camera"
+            " device.\n"
         )
 
-    if not depthai and plugin.type == DEPTHAI:
-        raise InvalidArgumentError(
-            "The cli flag 'depthai' is not set but the plugin you run is meant"
-            " for depthai usage."
-        )
 
-    # initialize real camera, to get frames
-    with device_handler.get_device() as r_cam:
-        # custom setup for depthai (on device handling)
-        if depthai:
-            r_cam.setup(plugin.device_setup, plugin.acquisition)
+@app.command(
+    help="List reset command to reset virtual camera devices",
+    context_settings={"help_option_names": ["-h", "--help"]},
+    rich_help_panel="General-Commands",
+)
+def reset_devices():
+    """List command on how to reset the added (virtual) camera devices."""
+    printer.reset_devices()
 
-        # initialize a virtual camera where the modified frames will be sent to
-        with pyvirtualcam.Camera(
-            width=r_cam.width,
-            height=r_cam.height,
-            fps=24,
-            device=virtual_path,
-        ) as v_cam:
-            # initialize a keyboard listener to get and use keystroke during runtime as trigger or switch
-            with KeyHandler() as listener:
-                listener.start()
 
-                # print in command line that the pipeline is running
-                device_handler.device_running()
-
-                # get frames from real camera, process it and sent it out via virtual camera
-                while True:
-                    # get a frame and optionally some on camera detections
-                    frame, detection = r_cam.get_frame()
-
-                    # convert bgr to rgb if <Ctrl+Alt+r> keys are pressed
-                    if listener.bgr2rgb_sw:
-                        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
-                    # pass trigger <Ctrl+Alt+f>, <Ctrl+Alt+l> and <Ctrl+Alt+n> for usage in plugin
-                    trigger = (
-                        listener.f_trig,
-                        listener.l_trig,
-                        listener.n_trig,
-                    )
-                    frame = plugin.process(frame, detection, trigger)
-
-                    # flip image if <Ctrl+Alt+m> keys are pressed
-                    if listener.mirror_sw:
-                        frame = cv2.flip(frame, 1)
-
-                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-                    # sent out the modified frame to the virtual camera
-                    v_cam.send(frame)
-                    v_cam.sleep_until_next_frame()
+@app.command(
+    help="Create new plugin",
+    context_settings={"help_option_names": ["-h", "--help"]},
+    rich_help_panel="General-Commands",
+    hidden=True,
+)
+def create_plugin():
+    """Create a new plugin"""
+    # TODO: Add a plugin creation template, to get started quickly.
+    raise NotImplementedError
 
 
 if __name__ == "__main__":
